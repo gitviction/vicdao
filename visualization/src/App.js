fd79320d7d182ec0c37fb113619334ba241afec8
geleeroyale <krassvs@gmail.com>
2019-03-10 02:56:01 +0100

    minimum viable UI

--- visualization/src/App.js
+++ visualization/src/App.js
@@ -1,21 +1,7 @@
-import React from 'react';
-import {
-  AppView,
-  AragonApp,
-  Button,
-  Info,
-  // Main,
-  Table,
-  TableHeader,
-  TableRow,
-  TableCell,
-  Text,
-  TextInput,
-  // observe
-} from "@aragon/ui";
+import React from "react";
+import ReactDOM from "react-dom";
 import axios from "axios";
 import styled from "styled-components";
-import GitterCritter from "./GitterCrittter";
 import {
   LineChart,
   Line,
@@ -26,9 +12,12 @@ import {
   YAxis
 } from "recharts";
 
-const AppContainer = styled(AragonApp)`
-  align-items: center;
-  justify-content: center;
+const VotingTable = styled.table``;
+
+const TableRow = styled.tr``;
+
+const TableData = styled.td`
+  padding: 2rem;
 `;
 
 const data = [
@@ -41,12 +30,6 @@ const data = [
   { name: "t7", conviction: 1, tokens: 0, total: 1 },
   { name: "t8", conviction: 0, tokens: 0, total: 0 }
 ];
-
-// const App = () => (
-//   <Main>
-//     <h1>Hello Aragon UI!</h1>
-//   </Main>
-// )
 
 export default class App extends React.Component {
   constructor(props) {
@@ -93,91 +76,44 @@ export default class App extends React.Component {
   render() {
     const issues = this.state.issues.map(issue => {
       return (
-        <TableRow key={issue.title}>
-          <TableCell>
-            <Text>{issue.title}</Text>
-          </TableCell>
-          <TableCell>
-            <Text>
+        <tbody>
+          <TableRow key={issue.title}>
+            <TableData>{issue.title}</TableData>
+            <TableData>
               {issue.amount} {issue.denomination}
-            </Text>
-          </TableCell>
-          <TableCell>
-            <LineChart
-              width={400}
-              height={150}
-              data={data}
-              margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
-            >
-              <Line type="monotone" dataKey="tokens" stroke="#000" />
-              <Line type="monotone" dataKey="conviction" stroke="#8884d8" />
-              <Line type="monotone" dataKey="total" stroke="#82ca9d" />
-              <CartesianGrid stroke="#ccc" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-            </LineChart>
-          </TableCell>
-          <TableCell>
-            <TextInput type="number" defaultValue="0" />
-            <Button
-              mode="outline"
-              onClick={e => {
-                this.startVote(issue.url);
-              }}
-            >
-              Vote
-            </Button>
-            {/* <GitterCritter issue={issue} app={this.props.app} /> */}
-          </TableCell>
-        </TableRow>
+            </TableData>
+            <TableData>
+              <LineChart
+                width={400}
+                height={150}
+                data={data}
+                margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+              >
+                <Line type="monotone" dataKey="tokens" stroke="#000" />
+                <Line type="monotone" dataKey="conviction" stroke="#8884d8" />
+                <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+                <CartesianGrid stroke="#ccc" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+              </LineChart>
+            </TableData>
+          </TableRow>
+        </tbody>
       );
     });
 
     return (
-      <AppContainer>
-        <AppView title="GitViction">
-          <Info background="#FFF9EB">
-            This App uses "conviction voting" to let members of the DAO vote on
-            prioritization and funding of GitHub issues. The novelty in the
-            voting scheme is a time component, where the token vote builds
-            "conviction" over time that gets added to a total token weight. Once
-            this total token weight reaches a treshold, a function is triggered
-            to create a bounty on GitCoin that is funded for the amount
-            requested in the issue.
-          </Info>
-          <Table>
-            <TableRow>
-              <TableHeader title="Issue" />
-              <TableHeader title="Funding" />
-              <TableHeader title="Voting Graph" />
-              <TableHeader title="Action" />
-            </TableRow>
-            {issues}
-          </Table>
-
-          <hr />
-
-          <Button
-            mode="outline"
-            onClick={e => {
-              this.getVote();
-            }}
-          >
-            get Vote
-          </Button>
-
-          <Button
-            mode="outline"
-            onClick={e => {
-              this.mintForMe();
-            }}
-          >
-            mint Voting tokens
-          </Button>
-        </AppView>
-      </AppContainer>
+      <VotingTable>
+        <thead>
+          <tr>
+            <th>Issue</th>
+            <th>Funding</th>
+            <th>Graph</th>
+          </tr>
+        </thead>
+        {issues};
+      </VotingTable>
     );
   }
 }
-
