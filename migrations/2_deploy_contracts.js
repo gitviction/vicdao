ae3c04e9ba7e57682f3f01f8ca41e32f11b5bf71
Loredana <loredana.cirstea@gmail.com>
2019-03-18 01:51:53 +0200

    Emit stake & withdraw events, fix constructor & deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,15 +1,10 @@
 var VictionToken = artifacts.require("VictionToken.sol");
 var GitViction = artifacts.require("GitViction.sol");
 
-module.exports = async function (deployer, network, accounts) {
-    const instance = await deployer.deploy(VictionToken);
-
-    console.log(`minting tokens for ${accounts[2]} , ${accounts[3]}`);
-    await Promise.all([
-        instance.mintFor(accounts[2], 100e18),
-        instance.mintFor(accounts[3], 150e18)
-    ]);
-
-    await deployer.deploy(GitViction);
-    GitViction.setToken(VictionToken.address, {from: accounts[0]});
+module.exports = function (deployer, network, accounts) {
+    deployer.deploy(VictionToken).then(function(token) {
+        token.mintFor(accounts[2], 100e18);
+        token.mintFor(accounts[3], 150e18);
+        return deployer.deploy(GitViction, VictionToken.address);
+    });
 };
